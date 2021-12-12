6a07ebb99e75b0df65087dc85251332bfde61f59
sanfrancesco <sanfrancesco.ai@gmail.com>
2021-12-13 00:41:26 +0100

    Added Base Project

---
+++ android/app/src/main/kotlin/com/besanfra/state_managers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.besanfra.state_managers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:state_managers/app.dart';
+
+void main() {
+  runApp(const App());
+}
