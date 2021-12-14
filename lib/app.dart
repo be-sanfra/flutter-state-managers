570e8e63f91a8fff69742214ab9f803f53be2543
sanfrancesco <sanfrancesco.ai@gmail.com>
2021-12-14 15:31:03 +0100

    Created 'Provider + ChangeNotifier' example

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:state_managers/pages/set_state_page.dart';
+import 'package:provider/provider.dart';
+import 'package:state_managers/models/counter.dart';
+import 'package:state_managers/pages/provider_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (contextChangeNotifierProvider) => Counter(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ProviderPage(),
       ),
-      home: const SetStatePage(),
     );
   }
 }
---
+++ lib/pages/provider_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_managers/models/counter.dart';
+
+class ProviderPage extends StatefulWidget {
+  const ProviderPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProviderPage> createState() => _ProviderPageState();
+}
+
+class _ProviderPageState extends State<ProviderPage> {
+
+  void _incrementCounter(BuildContext context) {
+    // setState(() {
+    //   ++_counter;
+    // });
+    var counter = context.read<Counter>();
+    counter.increment();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('State Manager - Provider + ChangeNotifier'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headline4,
+            // ),
+            Consumer<Counter>(
+              builder: (contextConsumer, counter, child) => Text(
+                '${counter.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // onPressed: () => context.read<Counter>().increment(),
+        onPressed: () => _incrementCounter(context),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
